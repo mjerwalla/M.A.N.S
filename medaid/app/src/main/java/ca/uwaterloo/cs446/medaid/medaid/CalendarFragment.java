4c739d70f26e7edb28ac62ffc5807b7f1f8eae1b
Nabil Barakati <nm.barakati@gmail.com>
2019-06-26 12:06:40 -0400

    Added calendar fragment and modified today fragment

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
@@ -14,44 +14,9 @@ import android.widget.TextView;
 import java.util.List;
 
 public class CalendarFragment extends Fragment {
-    private View v;
-    private CalendarActivityDBHelper medDb;
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.fragment_today, container, false);
-        medDb = new CalendarActivityDBHelper(getContext());
-        this.UpdateMedList();
-
-        return v;
-    }
-
-    private void UpdateMedList() {
-        LinearLayout linearLayout = v.findViewById(R.id.linearLayout);
-        List<CalendarActivityDBHelper.MyData> data = medDb.getAllData();
-
-        for (CalendarActivityDBHelper.MyData myData: data) {
-            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View rowView = inflater.inflate(R.layout.upcoming_med_block, linearLayout, false);
-
-            if (rowView.getParent() != null) {
-                ((ViewGroup)rowView.getParent()).removeView(rowView);
-            }
-            linearLayout.addView(rowView);
-
-            TextView medName = rowView.findViewById(R.id.txtMedName);
-            medName.setText(myData.column2);
-
-            TextView time = rowView.findViewById(R.id.txtTime);
-            time.setText(myData.column3.charAt(0) + ":00am");
-
-            TextView dosage = rowView.findViewById(R.id.txtDosage);
-            dosage.setText(myData.column7 + " pills");
-
-            // Set button ID
-            Button takenButton = rowView.findViewById(R.id.btnTaken);
-            takenButton.setId(Integer.parseInt(myData.column0));
-        }
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
 }
