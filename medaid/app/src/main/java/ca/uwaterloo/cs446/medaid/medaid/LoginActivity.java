86a25229486f933e62679d6db5dbe888b15d5191
NabilBarakati <nm.barakati@gmail.com>
2019-06-26 02:26:46 -0400

    Update LoginActivity.java

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/LoginActivity.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/LoginActivity.java
@@ -335,7 +335,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
 
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                Intent intent = new Intent(getBaseContext(), HistoryActivity.class);
                 startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
