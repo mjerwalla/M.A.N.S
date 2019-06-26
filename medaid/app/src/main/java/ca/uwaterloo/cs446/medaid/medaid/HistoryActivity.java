fb84443cfaef07c3253dcfaef8f2755f7419c7d5
Mufaddal Jerwalla <Mufaddal@v1040-wn-rt-c-74-179.campus-dynamic.uwaterloo.ca>
2019-06-26 05:30:46 -0400

    Added mock data

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/HistoryActivity.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/HistoryActivity.java
@@ -43,12 +43,14 @@ public class HistoryActivity extends AppCompatActivity {
             populateDate = dbHelper.insertData(1234, "Paracetamol", "1", "7", sdf.parse("2017-06-20 16:02"), sdf.parse("2017-06-30 16:02"), 1, 7, "Have after dinner");
             populateDate = dbHelper.insertData(1234, "Advil", "1", "2", sdf.parse("2017-06-23 16:02"), sdf.parse("2017-07-10 16:02"), 1, 10, "Have before sleeping");
             populateDate = dbHelper.insertData(1234, "Adderall", "2", "4", sdf.parse("2017-06-23 16:02"), sdf.parse("2017-07-19 16:02"), 1, 10, "Have before sleeping");
-            populateDate = dbHelper.insertData(1234, "Xanax", "1", "5", sdf.parse("2017-06-19 16:02"), sdf.parse("2017-06-30 16:02"), 1, 10, "Have before sleeping");
+            populateDate = dbHelper.insertData(1234, "Xanax", "1", "5", sdf.parse("2017-06-19 16:02"), sdf.parse("2017-06-30 16:02"), 1, 10, "Have before showering");
             populateDate = dbHelper.insertData(1234, "Ibuprofen", "1", "1", sdf.parse("2017-06-10 16:02"), sdf.parse("2017-08-10 16:02"), 1, 10, "Have before sleeping");
-            populateDate = dbHelper.insertData(1234, "Advil Flu and Cold", "1", "3", sdf.parse("2017-05-25 16:02"), sdf.parse("2017-06-28 16:02"), 1, 10, "Have before sleeping");
+            populateDate = dbHelper.insertData(1234, "Advil Flu and Cold", "1", "3", sdf.parse("2017-05-25 16:02"), sdf.parse("2017-06-28 16:02"), 1, 10, "Have before running");
             populateDate = dbHelper.insertData(1234, "Antibiotic", "2", "7", sdf.parse("2017-06-26 16:02"), sdf.parse("2017-07-07 16:02"), 1, 10, "Have before sleeping");
-            populateDate = dbHelper.insertData(1234, "Penicillin", "2", "2", sdf.parse("2017-06-22 16:02"), sdf.parse("2017-06-30 16:02"), 1, 10, "Have before sleeping");
-            populateDate = dbHelper.insertData(1234, "Panadol", "1", "7", sdf.parse("2017-06-25 16:02"), sdf.parse("2017-06-28 16:02"), 1, 10, "Have before sleeping");
+            populateDate = dbHelper.insertData(1234, "Penicillin", "2", "2", sdf.parse("2017-06-22 16:02"), sdf.parse("2017-06-30 16:02"), 1, 10, "Have before dinner");
+            populateDate = dbHelper.insertData(1234, "Panadol", "1", "7", sdf.parse("2017-06-25 16:02"), sdf.parse("2017-06-28 16:02"), 1, 10, "Have before lunch");
+            populateDate = dbHelper.insertData(1234, "Viagara", "1", "7", sdf.parse("2018-06-25 16:02"), sdf.parse("2019-06-28 16:02"), 1, 365, "Have every morning");
+
         } catch(Exception e) {
             System.out.println("Failed");
         }
