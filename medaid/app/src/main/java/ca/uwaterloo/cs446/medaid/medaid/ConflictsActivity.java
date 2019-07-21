9d846811dd5d8fd50ca275caf3dbb51a06ff2698
Aditi Shetty <aditi96@v1040-wn-rt-c-103-106.campus-dynamic.uwaterloo.ca>
2019-07-21 03:22:09 -0400

    medication to medication conflict checker working

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/ConflictsActivity.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/ConflictsActivity.java
@@ -2,7 +2,6 @@ package ca.uwaterloo.cs446.medaid.medaid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -21,14 +20,17 @@ public class ConflictsActivity extends AppCompatActivity {
         final String med2 = extras.getString("med2");
 
         final WebView webView = (WebView) findViewById(R.id.conflictView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://www.webmd.com/interaction-checker/default.htm");
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                webView.loadUrl("javascript:(function(){document.getElementById(\"ICDrugs-1\").value="+med1+";})()");
+                super.onPageFinished(webView, url);
+                System.out.println("running on PageFinished");
+                webView.loadUrl("javascript:(function(){document.getElementById('q').value='" + med1 + "; " + med2 + "';document.getElementsByClassName('btn btn-drugbank-primary btn-sm mr-2')[0].click();})()");
             }
         });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.drugbank.ca/interax/multi_search");
     }
 }
