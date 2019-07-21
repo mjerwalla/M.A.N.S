850b05472c1d2427e8d2d24d60c4b77783a39084
Aditi Shetty <aditi96@v1040-wn-rt-c-103-106.campus-dynamic.uwaterloo.ca>
2019-07-21 17:48:51 -0400

    Added analytics to Insights Fragment

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/InsightsFragment.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/InsightsFragment.java
@@ -1,8 +1,6 @@
 package ca.uwaterloo.cs446.medaid.medaid;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +11,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,20 +28,20 @@ import java.util.Date;
 public class InsightsFragment extends Fragment {
     View view;
     BarChart weekChart;
-    ArrayList Meds = new ArrayList();
-    ArrayList Day = new ArrayList();
+    ArrayList<BarEntry> Meds = new ArrayList();
+    ArrayList<String> Day = new ArrayList();
 
     //Will be removed
     String m = "[{'rowNum': '22', 'userID': '1', 'medName': 'Tylenol', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Dinner'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Advil', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Adderall', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Breakfast'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Advil', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,SAT', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Adderall', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'TUE,WED,THUR', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Breakfast'},\n" +
             "{'rowNum': 23', 'userID': '1', 'medName': 'Xanax', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Dinner'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Ibuprofen', 'startDate': '2018-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Advil Flu and Cold', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Evening Snack'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Antibiotic', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Breakfast'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Penicillin', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Dinner'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Ibuprofen', 'startDate': '2018-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'MON,THUR,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Advil Flu and Cold', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'SUN', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Evening Snack'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Antibiotic', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'SUN,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Breakfast'},\n" +
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Penicillin', 'startDate': '2019-07-02 00:00', 'endDate': '2019-08-01 00:00', 'selectedDaysPerWeek': 'SUN,THUR,SAT', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Dinner'},\n" +
             "{'rowNum': 23', 'userID': '1', 'medName': 'Panadol', 'startDate': '2017-07-02 00:00', 'endDate': '2017-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Breakfast'},\n" +
-            "{'rowNum': 23', 'userID': '1', 'medName': 'Buckleys', 'startDate': '2018-07-02 00:00', 'endDate': '2018-08-01 00:00', 'selectedDaysPerWeek': 'MON,WED,FRI', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'}]";
+            "{'rowNum': 23', 'userID': '1', 'medName': 'Buckleys', 'startDate': '2018-07-02 00:00', 'endDate': '2018-08-01 00:00', 'selectedDaysPerWeek': 'TUE,WED,SAT', 'numTimesPerDay': '3', 'timesToBeReminded': '8:00,14:00,21:00', 'takenWith': 'Lunch'}]";
 
 
     @Nullable
@@ -59,9 +61,6 @@ public class InsightsFragment extends Fragment {
                 String med1 = text1.getText().toString();
                 String med2 = text2.getText().toString();
 
-                System.out.println(med1);
-                System.out.println(med2);
-
                 Intent intent = new Intent(getContext(), ConflictsActivity.class);
                 Bundle extras = new Bundle();
                 extras.putString("med1", med1);
@@ -80,8 +79,6 @@ public class InsightsFragment extends Fragment {
             public void onClick(View view) {
 
                 String med = foodText.getText().toString();
-
-                System.out.println(med);
 
                 Intent intent = new Intent(getContext(), ConflictsActivity.class);
                 Bundle extras = new Bundle();
@@ -108,6 +105,14 @@ public class InsightsFragment extends Fragment {
         Day.add("SAT");
         Day.add("SUN");
 
+        int mon = 0;
+        int tue = 0;
+        int wed = 0;
+        int thur = 0;
+        int fri = 0;
+        int sat = 0;
+        int sun = 0;
+
         JSONArray medication;
 
         try {
@@ -123,18 +128,54 @@ public class InsightsFragment extends Fragment {
                 String s = x.getString("startDate");
                 String e = x.getString("endDate");
                 Calendar c = Calendar.getInstance();
+                c.add(Calendar.DATE, -1);
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
                 Date start = sdf.parse(s);
                 Date end = sdf.parse(e);
 
                 if ((c.getTimeInMillis() >= start.getTime()) && (c.getTimeInMillis() <= end.getTime())) {
-                    
-                }
+                    String selectedDays = x.getString("selectedDaysPerWeek");
 
+                    if (selectedDays.contains("MON")) {
+                        ++mon;
+                    }
+                    if (selectedDays.contains("TUE")) {
+                        ++tue;
+                    }
+                    if (selectedDays.contains("WED")) {
+                        ++wed;
+                    }
+                    if (selectedDays.contains("THUR")) {
+                        ++thur;
+                    }
+                    if (selectedDays.contains("FRI")) {
+                        ++fri;
+                    }
+                    if (selectedDays.contains("SAT")) {
+                        ++sat;
+                    }
+                    if (selectedDays.contains("SUN")) {
+                        ++sun;
+                    }
+                }
             } catch (Exception e) {
                 System.out.println("Failed to get JSON Object");
             }
         }
 
+        Meds.add(new BarEntry(mon, 0));
+        Meds.add(new BarEntry(tue, 1));
+        Meds.add(new BarEntry(wed, 2));
+        Meds.add(new BarEntry(thur, 3));
+        Meds.add(new BarEntry(fri, 4));
+        Meds.add(new BarEntry(sat, 5));
+        Meds.add(new BarEntry(sun, 6));
+
+        BarDataSet data = new BarDataSet(Meds, "No of Medicines");
+        weekChart.animateY(10);
+        BarData d = new BarData(Day, data);
+        data.setColors(ColorTemplate.COLORFUL_COLORS);
+        weekChart.setDescription("Number of Current Medication Per Day");
+        weekChart.setData(d);
     }
 }
