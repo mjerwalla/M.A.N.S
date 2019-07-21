750c837181e124aaef4b47e47b0884f77ae02b23
Nabil Barakati <nm.barakati@gmail.com>
2019-07-21 12:18:30 -0400

    functional decorator of a single day, need to call db to generate the list of days to decorate

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
@@ -1,6 +1,7 @@
 package ca.uwaterloo.cs446.medaid.medaid;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,13 +13,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.imanoweb.calendarview.CalendarListener;
-import com.imanoweb.calendarview.CustomCalendarView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,27 +38,34 @@ public class CalendarFragment extends Fragment {
     }
 
     public void whatev() {
-        CustomCalendarView calendarView = (CustomCalendarView) v.findViewById(R.id.calendar_view);
-        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
-        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
-        calendarView.refreshCalendar(currentCalendar);
+//        CustomCalendarView calendarView = (CustomCalendarView) v.findViewById(R.id.calendar_view);
+//        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+//        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
+//        List decorators = new ArrayList<>();
+//        decorators.add(new ColorDecorator());
+//        calendarView.setDecorators(decorators);
+//        calendarView.refreshCalendar(currentCalendar);
+//
+//        calendarView.setCalendarListener(new CalendarListener() {
+//            @Override
+//            public void onDateSelected(Date date) {
+//                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+//                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
+//            }
+//            @Override
+//            public void onMonthChanged(Date date) {
+//                SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
+//                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        MaterialCalendarView calendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
         List decorators = new ArrayList<>();
-        decorators.add(new ColorDecorator());
-        calendarView.setDecorators(decorators);
-
-        calendarView.setCalendarListener(new CalendarListener() {
-            @Override
-            public void onDateSelected(Date date) {
-                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onMonthChanged(Date date) {
-                SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
-                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        CalendarDay day = new CalendarDay();
+        HashSet<CalendarDay> days = new HashSet<CalendarDay>();
+        days.add(day);
+        decorators.add(new EventDecorator(0xFFFF0000, days));
+        calendarView.addDecorators(decorators);
+        calendarView.refreshDrawableState();
 
     }
 
