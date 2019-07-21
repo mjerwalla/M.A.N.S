979c48057ab6daf2d8266399541c89b9d6a4d541
Nabil Barakati <nm.barakati@gmail.com>
2019-07-21 10:30:22 -0400

    initial calender setup working

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/CalendarFragment.java
@@ -10,13 +10,52 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.imanoweb.calendarview.CalendarListener;
+import com.imanoweb.calendarview.CustomCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class CalendarFragment extends Fragment {
+    public View v;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        v = inflater.inflate(R.layout.fragment_calendar, container, false);
+        this.whatev();
+        return v;
     }
+
+    public void whatev() {
+        CustomCalendarView calendarView = (CustomCalendarView) v.findViewById(R.id.calendar_view);
+        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        calendarView.refreshCalendar(currentCalendar);
+        List decorators = new ArrayList<>();
+        decorators.add(new ColorDecorator());
+        calendarView.setDecorators(decorators);
+
+        calendarView.setCalendarListener(new CalendarListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onMonthChanged(Date date) {
+                SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
+                Toast.makeText(getContext(), df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
 }
