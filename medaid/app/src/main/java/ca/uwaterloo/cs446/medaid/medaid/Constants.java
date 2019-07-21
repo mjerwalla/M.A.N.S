e6e32ecb15f8618ed67a313f85afb54b822f7045
fokstephanie <the.stephanie.fok@gmail.com>
2019-07-21 19:51:42 -0400

    Fix typo, add POST fields

--- medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/Constants.java
+++ medaid/app/src/main/java/ca/uwaterloo/cs446/medaid/medaid/Constants.java
@@ -1,11 +1,13 @@
 package ca.uwaterloo.cs446.medaid.medaid;
 
 public final class Constants {
-    public static final String USER_ID = "userId";
+    public static final String USER_ID = "userID";
     public static final String MED_NAME = "medName";
     public static final String START_DATE = "startDate";
     public static final String END_DATE = "endDate";
     public static final String SELECTED_DAYS_PER_WEEK = "selectedDaysPerWeek";
     public static final String NUM_TIMES_PER_DAY = "numTimesPerDay";
     public static final String TIMES_TO_BE_REMINDED = "timesToBeReminded";
+    public static final String DOSAGE_PER_INTAKE = "dosagePerIntake";
+    public static final String TAKEN_IN_PAST = "takenInPast";
 }
